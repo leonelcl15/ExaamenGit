7c179519444b74c319e8cb28d51e92db5516b4fc
Leonel Cabanillas <lcabanillasl2501@educantabria.es>
2026-03-25 16:29:35 +0100

    primer commit realizado

---
+++ ExamenLeonel/src/main/java/examen/examenleonel/ExamenLeonel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package examen.examenleonel;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ExamenLeonel {
+
+    public static void main(String[] args) {
+        System.out.println("Leonel Jhairo Cabanillas López");
+    }
+}
